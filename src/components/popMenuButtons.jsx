6cff49fb93adf0bd517c0bd31c86a633511449c8
Sporadic <sporaclic@gmail.com>
2022-11-11 11:25:47 -0500

    popup menu styling, settings at initial game start

---
+++ src/components/popMenuButtons.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useContext } from 'react';
+import GameBrain from '../Context';
+import './styles/popMenuButton.css'
+
+export function PopMenuButton({ text }) {
+  const { state, dispatch } = useContext(GameBrain);
+
+
+  return (
+    <button
+      onClick={() => {
+        dispatch({ type: 'difficulty', payload: text });
+      }}
+      className={'hey'}
+      style={
+        state.difficulty.includes(text)
+          ? { backgroundColor: 'green', fontSize: '1.3rem' }
+          : { backgroundColor: 'gray', fontSize: '1.3rem' }
+      }
+    >
+      {text}
+    </button>
+  );
+}
+
+export function PopMenuCloseButton({isOpen,setIsOpen}){
+  const { state, dispatch } = useContext(GameBrain);
+
+  function handleClick(){
+
+  dispatch({type:'startMenu', payload:false})
+  setIsOpen(false)
+  // dispatch({type:'startMenu', payload:false})
+
+  }
+
+
+  return(
+
+  <button onClick={handleClick} className='closeButton'>{state.startMenu ? 'Start Game' : 'Continue'}</button>
+  
+  )
+
+}
--- src/components/settingsLine.jsx
+++ src/components/settingsLine.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 export default function Setting({description,settings}) {
   return (
     <>
-      <div className='settingLineLeft'><h2>{description}:</h2></div>
-      <div className='settingLineRight'>{settings}</div>
+      <div className='settingLineTop'><h2>{description}</h2></div>
+      <div className='statsDivider' > </div>
+      <div className='settingLineBottom'>{settings}</div>
+
     </>
   );
 }
